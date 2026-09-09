b61bd0e2c5d04408f36a823c8179a0153e36b6bb
guntu <guntu@DESKTOP-A8OO8BO>
2026-09-09 21:07:08 +0530

    Type conversions

---
+++ Typeconversions/src/Com/Typeconversions/TypeMain.java
@@ -0,0 +1,12 @@
+package Com.Typeconversions;
+
+public class TypeMain {
+
+	public static void main(String[] args) {
+		TypeDemo td = new TypeDemo();
+		td.implicittype();
+		td.explicittype();
+
+	}
+
+}
